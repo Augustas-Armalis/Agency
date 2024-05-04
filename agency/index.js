98104e823b20d9a6d547af897624e282ca1049bc
Augustas Armalis <119808410+Augustas-Armalis@users.noreply.github.com>
2024-05-04 17:42:02 +0300

    testimonials and faqs index.js

--- agency/index.js
+++ agency/index.js
@@ -1,108 +1,108 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   document.body.style.overflow = "hidden";
-//   setTimeout(function () {
-//     document.body.style.overflow = "auto";
-//   }, 4000);
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.style.overflow = "hidden";
+  setTimeout(function () {
+    document.body.style.overflow = "auto";
+  }, 4000);
+});
 
-// window.onload = function () {
-//   const bars = document.querySelector(".bars");
-//   const menu = document.querySelector(".menu");
-//   const navItems = document.querySelectorAll(".navigation-item");
-//   let menuOpen = false;
-//   let lenisInstance;
+window.onload = function () {
+  const bars = document.querySelector(".bars");
+  const menu = document.querySelector(".menu");
+  const navItems = document.querySelectorAll(".navigation-item");
+  let menuOpen = false;
+  let lenisInstance;
 
-//   function initializeLenis() {
-//     lenisInstance = new Lenis();
-//     lenisInstance.on('scroll', (e) => {
-//       console.log(e);
-//     });
-//     function raf(time) {
-//       lenisInstance.raf(time);
-//       requestAnimationFrame(raf);
-//     }
-//     requestAnimationFrame(raf);
-//   }
+  function initializeLenis() {
+    lenisInstance = new Lenis();
+    lenisInstance.on('scroll', (e) => {
+      console.log(e);
+    });
+    function raf(time) {
+      lenisInstance.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }
 
-//   setTimeout(initializeLenis, 5000);
+  setTimeout(initializeLenis, 5000);
 
-//   bars.addEventListener("click", function (e) {
-//     this.classList.toggle("active");
-//     menuOpen = !menuOpen;
-//     if (menuOpen) {
-//       gsap.to(".menu", {
-//         duration: 1,
-//         display: "flex",
-//         ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)"
-//       });
-//       gsap.fromTo(".nav", {
-//         marginTop: "-100vh"
-//       }, {
-//         duration: 1,
-//         marginTop: "0",
-//         ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)",
-//         onComplete: function () {
-//           gsap.to(".navigation-item", {
-//             duration: 0,
-//             opacity: 1
-//           });
-//           gsap.to(".navigation-item", {
-//             duration: 0.6,
-//             y: 0,
-//             ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)",
-//             stagger: 0.1
-//           });
-//         }
-//       });
-//       document.body.style.overflow = 'hidden';
-//       if (lenisInstance) {
-//         lenisInstance.destroy();
-//         lenisInstance = null;
-//       }
-//     } else {
-//       gsap.to(".navigation-item", {
-//         duration: 0.5,
-//         y: "-100%",
-//         ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)",
-//         stagger: 0.1,
-//         onComplete: function () {
-//           gsap.to(".navigation", {
-//             duration: 0,
-//             opacity: 0
-//           });
-//           gsap.to(".nav", {
-//             duration: 1,
-//             marginTop: "-100vh",
-//             ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)"
-//           });
-//           gsap.to(".menu", {
-//             duration: 1,
-//             display: "none",
-//             ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)"
-//           });
-//         }
-//       });
-//       document.body.style.overflow = 'auto';
-//       initializeLenis();
-//     }
-//   });
-// };
+  bars.addEventListener("click", function (e) {
+    this.classList.toggle("active");
+    menuOpen = !menuOpen;
+    if (menuOpen) {
+      gsap.to(".menu", {
+        duration: 1,
+        display: "flex",
+        ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)"
+      });
+      gsap.fromTo(".nav", {
+        marginTop: "-100vh"
+      }, {
+        duration: 1,
+        marginTop: "0",
+        ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)",
+        onComplete: function () {
+          gsap.to(".navigation-item", {
+            duration: 0,
+            opacity: 1
+          });
+          gsap.to(".navigation-item", {
+            duration: 0.6,
+            y: 0,
+            ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)",
+            stagger: 0.1
+          });
+        }
+      });
+      document.body.style.overflow = 'hidden';
+      if (lenisInstance) {
+        lenisInstance.destroy();
+        lenisInstance = null;
+      }
+    } else {
+      gsap.to(".navigation-item", {
+        duration: 0.5,
+        y: "-100%",
+        ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)",
+        stagger: 0.1,
+        onComplete: function () {
+          gsap.to(".navigation", {
+            duration: 0,
+            opacity: 0
+          });
+          gsap.to(".nav", {
+            duration: 1,
+            marginTop: "-100vh",
+            ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)"
+          });
+          gsap.to(".menu", {
+            duration: 1,
+            display: "none",
+            ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)"
+          });
+        }
+      });
+      document.body.style.overflow = 'auto';
+      initializeLenis();
+    }
+  });
+};
 
-// gsap.from(".split", {
-//   yPercent: 100,
-//   duration: 0.5,
-//   ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)",
-//   stagger: 0.05,
-//   delay: 1,
-//   onComplete: function () {
-//     gsap.to(".split", {
-//       yPercent: 1000,
-//       duration: 1,
-//       delay: 1,
-//       ease: "power1.inOut"
-//     });
-//   }
-// });
+gsap.from(".split", {
+  yPercent: 100,
+  duration: 0.5,
+  ease: "cubic-bezier(0.770, 0.000, 0.175, 1.000)",
+  stagger: 0.05,
+  delay: 1,
+  onComplete: function () {
+    gsap.to(".split", {
+      yPercent: 1000,
+      duration: 1,
+      delay: 1,
+      ease: "power1.inOut"
+    });
+  }
+});
 
 gsap.set(".agency-a", { x: -130 });
 gsap.to(".agency-a", {
